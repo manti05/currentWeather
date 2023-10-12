ee786d93d0a63d482d434786aa94e41f125c9c17
manti05 <snapmantas@gmail.com>
2023-10-12 13:34:49 +0100

    Improved the code and separated it into more functions

--- app.py
+++ app.py
@@ -1,86 +1,110 @@
-import config
+# Import necessary modules
 import requests
-import tkinter as tk
+import config
 
 
-def get_input():
-	# City and Weather Data
-	city_input = input("Enter city: ")
-
-	# Create a variable for you api key inside the config file or here
-	location_data = requests.get(
-		f"http://api.openweathermap.org/geo/1.0/direct?q={city_input}&limit=5&appid={config.api_key}"
-	)
-	# Country
-	countries = []
-	# Iterating over the list
-	for i, val in enumerate(location_data.json()):
-		# Adding a country to a list
-		if val['country']:
-			countries.append(val['country'])
-			# Statement breaks when the list item doesn't have a state
-			# Adding the name of the state if the city is in the US
-			# if :
-			# 	countries[i] = val['state'] + ", " + val['country']
-
-	# Removing duplicating countries until I find a solution for selecting states
-	dup_free_countries = list(set(countries))
-
-	# If there are multiple countries with the same city
-	if len(dup_free_countries) > 1:
-		# Listing all countries with the same city name
-		for country in dup_free_countries:
-			print(country)
-		# Selecting the country
-		country_input = input("Enter country: ")
-		# state_input = input("Enter state: ")
-		weather_data = requests.get(
-			f"https://api.openweathermap.org/data/2.5/weather?q={city_input},{country_input}&units=metric&appid={config.api_key}"
-		)
-		# using get_weather function
-		get_weather(city_input, weather_data)
-
-	else:
-		weather_data = requests.get(
-			f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=metric&appid={config.api_key}"
-		)
-		# using get_weather function
-		get_weather(city_input, weather_data)
+# Function to fetch location data for a given city
+def get_location_data(city):
+	try:
+		# Send an API request to get location data based on the city
+		location_data = requests.get(
+			f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={config.api_key}")
+		location_data.raise_for_status()
+		return location_data.json()  # Return the JSON data received
+	except requests.exceptions.RequestException as e:
+		print(f"Error fetching location data: {e}")
+		return None
 
 
-def get_weather(city_input, weather_data):
-	# Checking if a valid city name was input
-	if weather_data.json()['cod'] == '404':
+# Function to prompt the user to select a country code when multiple countries have the same city name
+def get_country_code(city, countries):
+	print("Multiple countries with the same city name. Please select a country by its country code:")
+	for i, country in enumerate(countries, start=1):
+		print(f"{i}. {country['country']} ({country['name']})")
+
+	while True:
+		try:
+			choice = int(input("Enter the number corresponding to your choice: "))
+			if 1 <= choice <= len(countries):
+				return countries[choice - 1]['country']  # Return the selected country code
+			else:
+				print("Invalid choice. Please select a valid number.")
+		except ValueError:
+			print("Invalid input. Please enter a number.")
+
+
+# Function to fetch weather data for a city and country (if specified)
+def get_weather_data(city, country=None):
+	base_url = "https://api.openweathermap.org/data/2.5/weather"
+	params = {"q": city, "units": "metric", "appid": config.api_key}
+	if country:
+		params["q"] = f"{city},{country}"
+	try:
+		# Send an API request to get weather data for the city and country (if specified)
+		weather_data = requests.get(base_url, params=params)
+		weather_data.raise_for_status()
+		return weather_data.json()  # Return the JSON data received
+	except requests.exceptions.RequestException as e:
+		print(f"Error fetching weather data: {e}")
+		return None
+
+
+# Function to display weather information for a city
+def display_weather(city, country, weather_data):
+	# print("Received weather data:")
+	# print(weather_data)
+	if 'cod' in weather_data and weather_data['cod'] == 404:
+		# Check if the API request is valid; if not, display an error message
 		print("Invalid city")
 	else:
-		# Country
-		country = weather_data.json()['sys']['country']
-
+		# Extract and display weather information
 		# Weather
-		weather = weather_data.json()['weather'][0]['main']
-		description = weather_data.json()['weather'][0]['description']
+		weather = weather_data['weather'][0]['main']
+		description = weather_data['weather'][0]['description']
 
 		# Temperatures in °C
-		temp = round(weather_data.json()['main']['temp'])
-		feel = round(weather_data.json()['main']['feels_like'])
-		# temp_min = round(weather_data.json()['main']['temp_min'])
-		# temp_max = round(weather_data.json()['main']['temp_max'])
+		temp = round(weather_data['main']['temp'])
+		feel = round(weather_data['main']['feels_like'])
 
 		# Wind in m/s
-		wind = weather_data.json()['wind']['speed']
+		wind = weather_data['wind']['speed']
 
 		# Humidity %
-		humidity = weather_data.json()['main']['humidity']
+		humidity = weather_data['main']['humidity']
+		print("Finished assigning variables")
 
-		print(f"{city_input}, {country}")
+		print(f"{city}, {country}")
 		print()
-		print(f"The current weather in {city_input} is: {weather} - {description}")
+		print(f"The current weather in {city} is: {weather} - {description}")
 		print(f"The temperature is: {temp}°C feels like: {feel}°C")
-		# print(f"The minimum: {temp_min}°C and maximum: {temp_max}°C today ")
 		print(f"The humidity is: {humidity}%")
 		print(f"The wind speed is: {wind} m/s")
 		print()
 
 
+# Main function to coordinate the program
+def main():
+	city = input("Enter city: ")
+	location_data = get_location_data(city)
+	selected_country_code = None  # Initialize to None
+
+	if location_data:
+		countries = [val for val in location_data if val["country"]]
+		if len(countries) > 1:
+			print("Multiple countries with the same city name. Please specify the country.")
+			selected_country_code = get_country_code(city, countries)
+			print(f"Selected country code: {selected_country_code}")
+
+		weather_data = get_weather_data(city, selected_country_code)
+
+		if weather_data:
+			display_weather(city, selected_country_code, weather_data)
+		else:
+			print("Failed to fetch weather data.")
+	else:
+		print("Failed to fetch location data.")
+
+
+# Entry point of the program
 if __name__ == '__main__':
-	get_input()
+	main()
